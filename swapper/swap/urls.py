3c99765dfa9cbb09d83421678a1bddb7bb217b28
Crewch <artemkr2003@mail.ru>
2022-07-08 22:46:28 +0300

    THE BIGGEST UPDATA!!!!!!!!!!!!!!!!!!!!

--- swapper/swap/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('about-us', views.about, name='about'),
-    path('create', views.create, name='create'),
-    path("signup/", views.SignUp.as_view(), name="signup"),
-]
